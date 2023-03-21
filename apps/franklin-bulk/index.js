e585ddc3efc320b3dc880013579e21e13f7752cc
David Catalan <catalan@adobe.com>
2023-03-21 08:37:04 +0100

    adapt cli help message to terminal width

--- apps/franklin-bulk/index.js
+++ apps/franklin-bulk/index.js
@@ -1,9 +1,12 @@
 #!/usr/bin/env node
 
+const yargs = require('yargs');
+
 (async function main() {
-  await require('yargs/yargs')(process.argv.slice(2))
+  await yargs(process.argv.slice(2))
     .commandDir('cmds')
     .demandCommand()
     .help('h')
+    .wrap(yargs.terminalWidth())
     .argv;
 })();
