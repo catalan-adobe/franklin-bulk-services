8ae6735fc2a7f9c63d2f6ea25810d116d8872735
David Catalan <catalan@adobe.com>
2023-05-03 16:10:59 +0200

    cleanup

--- shared/src/puppeteer/steps/steps.ts
+++
@@ -1,9 +0,0 @@
-
-export * from './step-post-load-wait.js';
-export * from './step-smart-scroll.js';
-export * from './step-franklin-import.js';
-export * from './step-collect-webconsole.js';
-export * from './step-fullpage-screenshot.js';
-export * from './step-gdpr-autoconsent.js';
-export * from './step-exec-async.js';
-export * from './step-run-lighthouse.js';
