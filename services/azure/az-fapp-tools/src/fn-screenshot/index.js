b560c7661ede9208959ff8fa5b2c295e46f33536
David Catalan <catalan@adobe.com>
2023-08-25 15:21:33 +0200

    refactor: use franklin-bulk-shared in screenshot function

--- services/azure/az-fapp-tools/src/fn-screenshot/index.js
+++ services/azure/az-fapp-tools/src/fn-screenshot/index.js
@@ -1,65 +1,93 @@
-import * as importerLib from 'franklin-bulk-shared';
+import * as frkBulk from 'franklin-bulk-shared';
+
+const DEFAULT_INPUT_PARAMETERS = {
+    url:            "https://google.com/",
+    width:          1280,
+    adBlocker:      true,
+    gdprBlocker:    true,
+    delay:          0,
+    jsToInject:     null,
+};
 
 export async function main(context, req) {
+
+    context.log("Request: ", req);
+
+    const options = {
+        ...DEFAULT_INPUT_PARAMETERS,
+        ...req.body,
+    };
+
+    context.log('options', options);
+
+    if (req.method === 'GET') {
+        context.res = {
+            body: USAGE_MESSAGE,
+            headers: {
+                "content-type": "text/plain",
+            }
+        };
+        context.done();
+        return new Promise(() => {}).resolve();
+    }
+
     let browser;
     let page;
+    let screenshotBuffer;
 
     try {
-        [browser, page] = await importerLib.Puppeteer.initBrowser({ headless: true });
-            
-        const url = req.query.url || "https://google.com/";
-        const width = req.query.width ? parseInt(req.query.width, 10) : 1280;
-        const postLoadWait = req.query.delay ? parseInt(req.query.delay, 10) : 1000;
-
-        await page.setViewport({
-            width: width,
-            height: 1000,
-            deviceScaleFactor: 1,
+        [browser, page] = await frkBulk.Puppeteer.initBrowser({
+            width: options.width,
+            adBlocker: options.adBlocker,
+            gdprBlocker: options.gdprBlocker,
+            headless: false,
         });
     
-        console.log(`Loading ${url}...`);
-        console.log(`Viewport: ${width}x1000`);
+        await page.goto(options.url, { waitUntil: 'networkidle0' });
+        
+        if (options.delay > 0) {
+            await frkBulk.Time.sleep(options.delay);
+        }
+        
+        if (options.jsToInject) {
+            try {
+                let navigationTriggered = false;
+                await page.setRequestInterception(true);
+                page.on('request', interceptedRequest => {
+                    if (interceptedRequest.isNavigationRequest()) {
+                        context.log(interceptedRequest);
+                        navigationTriggered = true;
+                    }
+                    interceptedRequest.continue();
+                });
+                          
+                const js = decodeURIComponent(options.jsToInject);
+                await page.evaluate(js);
+                await frkBulk.Time.sleep(100);
+                if (navigationTriggered) {
+                    await page.waitForNavigation({ waitUntil: 'networkidle0' });
+                }
+            } catch (e) {
+                context.log.error(e);
+            }
+        }
+        
+        await frkBulk.Puppeteer.smartScroll(page, { postReset: true });
 
-        let screenshotBuffer;
+        // page height in browser 
+        const pageHeight = await page.evaluate(() =>  window.document.body.offsetHeight || window.document.body.scrollHeight);
 
-        await importerLib.Puppeteer.runStepsSequence(
-            page,
-            url,
-            [
-                importerLib.Puppeteer.Steps.postLoadWait(500),
+        screenshotBuffer = await page.screenshot({
+            clip: {
+                x: 0,
+                y: 0,
+                width: options.width,
+                height: pageHeight,
+            },
+        });
 
-                importerLib.Puppeteer.Steps.GDPRAutoConsent(),
-                
-                importerLib.Puppeteer.Steps.execAsync(async (browserPage) => {
-                    await browserPage.keyboard.press('Escape');
-                }),
+        context.log(`All done. ✨`);
 
-                importerLib.Puppeteer.Steps.execAsync(async (browserPage) => {
-                    // scroll to bottom
-                    await browserPage.evaluate(() => {
-                    window.scrollTo({ left: 0, top: window.document.body.scrollHeight, behavior: 'smooth' });
-                    });
-                    await importerLib.Time.sleep(2000);
-              
-                    // scroll bsck up
-                    await browserPage.evaluate(() => {
-                    window.scrollTo(0, 0);
-                    });
-                    await importerLib.Time.sleep(250);
-                }),
-
-                importerLib.Puppeteer.Steps.execAsync(async (browserPage) => {
-                    await importerLib.Time.sleep(postLoadWait);
-                }),
-
-                importerLib.Puppeteer.Steps.execAsync(async (browserPage) => {
-                    screenshotBuffer = await browserPage.screenshot({ fullPage: true });
-                }),
-            ],
-        );
-
-        await browser.close();
-    
         context.res = {
             body: screenshotBuffer,
             headers: {
@@ -73,7 +101,40 @@ export async function main(context, req) {
         };
     } finally {
         if (browser) {
+            context.log.info("Closing browser..");
             await browser.close();
         }
     }
 };
+
+const USAGE_MESSAGE = `
+Takes a screenshot of a webpage
+
+Method: POST
+
+JSON Input Body:
+
+    {
+        url:            "<url>",    // required
+        width:          <width>,    // the viewport width of the browser (in pixels)
+                                    // default: 1280
+        adBlocker:      true|false, // enable ad blocker plugin
+                                    // default: true
+        gdprBlocker:    true|false, // enable gdpr blocker plugin
+                                    // default: true
+        delay:          <delay>,    // forced waiting time after page load (in ms.)
+                                    // default: 0 (no delay)
+        jsToInject:     "<js_code>",     // javascript code injected before taking the screenshot
+                                    // default: null
+    }
+
+    Example
+
+    {
+        url:            "https://www.adobe.com",
+        width:          1440,
+        adBlocker:      true,
+        gdprBlocker:    false,
+        jsToInject:     "document.querySelector('locale-modal a.dexter-CloseButton').click();"
+    }
+`;
